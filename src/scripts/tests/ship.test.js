73d11f02d8d9c8c4a0cb08a602d6ff70b38e8751
Denzel Granadoz <zelgranadoz@gmail.com>
2022-07-07 21:53:30 +0800

    updated tests

--- src/scripts/tests/ship.test.js
+++ src/scripts/tests/ship.test.js
@@ -1,18 +1,18 @@
 import Ship from '../ship';
 
 test('if ship has correct length', () => {
-  const battleship = new Ship(4);
+  const battleship = Ship(4);
   expect(battleship.length).toBe(4);
 });
 
 test('whether the ship is hit in right position', () => {
-  const carrier = new Ship(5);
+  const carrier = Ship(5);
   carrier.hit(2);
   expect(carrier.shipBody[2]).toBe('hit');
 });
 
 test('ship has been sunk or not', () => {
-  const submarine = new Ship(3);
+  const submarine = Ship(3);
   submarine.hit(0);
   submarine.hit(1);
   submarine.hit(2);
